a5919c5b7b01a792b12b8e807f564ee09df81631
bbrown <bennyc.brown@gmail.com>
2017-05-18 07:24:59 -0700

    FibonacciSequence

---
+++ Fibonacci/ConsoleApplication1/FibRunner.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    /// <summary>
+    /// Calculates the fibbonacci sequence number.
+    /// Presumes the sequence starts out: [1, 2, 3, 5 ...]
+    /// </summary>
+    /// <remarks>
+    /// To make the sequence start [0,1,2,3...] set <see cref="Initial"/> to be zero and <see cref="PreviousInitial"/> to be one.
+    /// To make the sequence start [1,1,2,3...] set <see cref="Initial"/> to be 1 and <see cref="PreviousInitial"/> to be zero.
+    /// </remarks>
+    public class FibRunner
+    {
+        /// <summary>
+        /// The first number in the fibbonacci sequence.
+        /// </summary>
+        public const int Initial = 1;
+
+        /// <summary>
+        /// The first 'previous' number to add in the sequence.
+        /// </summary>
+        public const int PreviousInitial = 1;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting fibbonacci sequence");
+
+            Console.WriteLine($"Fibbonacci sequence 10: {Fib(10)}");
+
+            Console.WriteLine($"Fibbonacci sequence 40: {Fib(40)}");
+        }
+
+        public static int Fib(int position)
+        {
+            int current = Initial;
+            int prev = PreviousInitial;
+            int temp = 0;
+
+            for (int i = 1; i < position; i++)
+            {
+                temp = current;
+                current = current + prev;
+                prev = temp;
+            }
+
+            return current;
+        }
+    }
+}
---
+++ Fibonacci/Tests/Tests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApplication1;
+using Xunit;
+
+namespace Tests
+{
+    public class Test
+    {
+        [InlineData(1, 1)]
+        [InlineData(2, 2)]
+        [InlineData(5, 8)]
+        [InlineData(10, 89)] 
+        [InlineData(20, 10946)]
+        [InlineData(40, 165580141)]
+        [Theory]
+        public void CheckFib(int position, int expected)
+        {
+            int actual = FibRunner.Fib(position);
+             Assert.Equal(expected, actual);
+        }
+    }
+}
